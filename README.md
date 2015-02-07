623e072c192d513e7c9a3d7df2717cc2951edb66
Samy Pessé <samypesse@gmail.com>
2015-02-07 15:34:44 +0100

    Base code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# alerts-mail
-Mail alerts for reportr
+# Mail Alerts for Reportr
+
+### How to add it to your instance?
+
+Add `reportr-alerts-ail` to the `package.json` and load it in your Reportr configuration:
+
+```js
+reportr.configure({
+    alerts: [
+        require("reportr-alerts-mail")({
+            'service': process.env.MAIL_SERVICE,
+            'auth': {
+                'user': process.env.MAIL_USERNAME,
+                'password': process.env.MAIL_PASSWORD
+            },
+            'from': process.env.MAIL_FROM
+        })
+    ]
+});
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+var Q = require('q');
+var _ = require('lodash');
+var nodemailer = require("nodemailer");
+
+module.exports = function(options) {
+    var exec = function(alert, alertConfig, e) {
+        var deferred = Q.defer();
+
+        var transport = nodemailer.createTransport("SMTP", {
+            service: options.service,
+            auth: options.auth
+        });
+
+        var content = [
+            "Alert Notification for <b>"+alert.eventName+"</b>"
+        ].join("<br/>\n");
+
+        transport.sendMail({
+            from: options.from,
+            to: alertConfig.to,
+            subject: "Reportr - Alert - "+alert.eventName,
+            text: "",
+            html: content
+        }, function(error, response){
+            if (error) {
+                deferred.reject(error);
+            } else {
+                deferred.resolve();
+            }
+        });
+
+        return deferred.promise;
+    };
+
+    return {
+        id: "mail",
+        execute: exec
+    };
+};
